bbfafdfd22f6c2831cbd28db4584da390c45bd7c
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-07-18 07:53:46 +0000

    いいね機能実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :profile_image
+  has_many :favorites, dependent: :destroy
+  
   
   def get_profile_image(width,height)
     unless profile_image.attached?
@@ -12,4 +14,9 @@ class Book < ApplicationRecord
   
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+  
 end
