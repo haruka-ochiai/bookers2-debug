45bfac5a808c22cb6daa954a3a779b47afd406fc
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-07-22 12:38:48 +0000

    メソッド作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,26 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
-  has_many :relationships, class_name: "relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :reverse_of_relationships, class_name: "relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   
-  has_many :following, through: :relationships, source: :followed
-  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :following_users, through: :followers, source: :followed
+  has_many :follower_users, through: :followeds, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
+  def follow(user_id)
+    followers.create(followed_id: user_id)
+  end
   
+  def unfollow(user_id)
+    followers.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    following_users.include?(user)
+  end
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
