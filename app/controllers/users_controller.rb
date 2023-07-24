7c0bba3b4fe4494f7a5d698209bda3eea19fb7ca
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-07-24 07:42:06 +0000

    finish 応用課題4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   
   def followers
     user = User.find(params[:id])
-    @user = user.follower_users
+    @users = user.follower_users
   end
 
   private
