aca5fbc42f2937418960017bc003d9dce3e5446c
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-10-20 02:45:09 +0000

    add controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,6 +10,7 @@ class Book < ApplicationRecord
   validates :body,presence:true,length:{maximum:200}
   validates :tag, presence:true
   
+  # 日付ごとに並び替え
   scope :latest, -> { order(created_at: :desc)}
   scope :old, -> { order(created_at: :asc)}
   scope :most_favorites, -> {
@@ -17,6 +18,12 @@ class Book < ApplicationRecord
     .group('book_id')
     .order('COUNT(favorites.id) DESC')
   }
+  
+  # 日ごとの投稿数
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } 
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } 
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } 
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } 
   
   def get_profile_image(width,height)
     unless profile_image.attached?
