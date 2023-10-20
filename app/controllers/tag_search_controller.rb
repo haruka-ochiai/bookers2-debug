ef57cae02de418e4d4b7d47c0b5dae2261c18ce5
haruka-ochiai <138193229+haruka-ochiai@users.noreply.github.com>
2023-10-20 04:10:51 +0000

    fix tag

--- app/controllers/tag_search_controller.rb
+++ app/controllers/tag_search_controller.rb
@@ -1,8 +1,8 @@
 class TagSearchController < ApplicationController
-    
+
   def search
     @model = Book
-    @word = params[:word]
+    @word = params[:content]
     @books = Book.where("tag LIKE?", "%#{@word}%")
     render "tag_search/tagsearch"
   end
